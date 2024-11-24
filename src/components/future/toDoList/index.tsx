802b29c4a265c24fb05c2eec0382768d812337f0
seyed <iseyedmahdihosseini313@gamil.com>
2024-11-24 04:04:01 +0330

    fix conflict

--- src/components/future/toDoList/index.tsx
+++ src/components/future/toDoList/index.tsx
@@ -1,9 +1,5 @@
 'use client';
-<<<<<<< HEAD
 import React, { useRef, useState } from 'react';
-=======
-import React, { useState } from 'react';
->>>>>>> ec0a5acf30a5682f9c73edc8019c9d05f4bd384a
 import { BloomFilter } from 'bloomfilter';
 import { AddSearchBox, FilterSearchBox, TodoListBox } from '@/components';
 import ITodos from '@/types/todos.type';
@@ -11,7 +7,7 @@ const bloom = new BloomFilter(32 * 256, 16); // Filter bloom bar asase size va h
 const TodoList = () => {
      const [todos, setTodos] = useState<ITodos>([]);
      const [filter, setFilter] = useState('');
-     const addRef=useRef<HTMLInputElement>(null)
+     const addRef = useRef<HTMLInputElement>(null);
      return (
           <div className="bg-black min-w-[100vw] min-h-screen flex flex-col gap-2 justify-center items-center p-5 ">
                <FilterSearchBox setFilter={setFilter} />
